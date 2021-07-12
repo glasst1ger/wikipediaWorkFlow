0827c420bee3066ca2bae9e0c14c7619c945b549
appuser <appuser@journey2-5bd6d4594b-pnfcz>
2021-07-12 07:49:41 +0000

    starter project

---
+++ src/main/test/java/com/codecool/wikipediatest/TestingWikipedia.java
@@ -0,0 +1,7 @@
+package main.test.java.com.codecool.wikipediatest;
+
+public class TestingWikipedia {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/codecool/wikipediatest/WikipediaTest.java
@@ -0,0 +1,11 @@
+package com.codecool.wikipediatest;
+
+import org.junit.jupiter.api.Test;
+
+public class WikipediaTest {
+
+    @Test
+    public void testWikipedia() {
+
+    }
+}
