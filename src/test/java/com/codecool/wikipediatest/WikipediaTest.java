c6c1a69a3346bb413610d844c9efecc9e8e937d9
glasst1ger <szak.viktor@gmail.com>
2021-07-29 18:19:28 +0200

    test without implicit wait

--- src/test/java/com/codecool/wikipediatest/WikipediaTest.java
+++ src/test/java/com/codecool/wikipediatest/WikipediaTest.java
@@ -40,7 +40,6 @@ public class WikipediaTest {
         options.addArguments("--headless");
         webDriver = new ChromeDriver(options);
 
-        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
     }
 
